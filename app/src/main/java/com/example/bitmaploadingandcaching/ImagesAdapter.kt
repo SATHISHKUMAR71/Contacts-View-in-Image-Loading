63782241afa76468d9ff7deae020d593aa81d95c
sathishkumar <satish102719@gmail.com>
2024-08-13 17:58:30 +0530

    Change in Layout

--- app/src/main/java/com/example/bitmaploadingandcaching/ImagesAdapter.kt
+++ app/src/main/java/com/example/bitmaploadingandcaching/ImagesAdapter.kt
@@ -9,6 +9,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.collection.LruCache
 import androidx.recyclerview.widget.RecyclerView
 import com.facebook.shimmer.ShimmerFrameLayout
@@ -16,13 +17,14 @@ import kotlinx.coroutines.delay
 import java.net.URL
 import java.sql.Time
 
-class ImagesAdapter(private var imageList:MutableList<String>,private var context: Context):RecyclerView.Adapter<ImagesAdapter.ImageHolder>(){
+class ImagesAdapter(private var imageList:MutableList<Contact>,private var context: Context):RecyclerView.Adapter<ImagesAdapter.ImageHolder>(){
     private var i =0
     private var bitmapCache = LruCache<String,Bitmap>((Runtime.getRuntime().maxMemory()/4).toInt())
 
     inner class ImageHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
 
-        val imageView: ImageView = itemView.findViewById<ImageView>(R.id.imageView2)
+        val imageView: ImageView = itemView.findViewById(R.id.contactImage)
+        val contactName:TextView = itemView.findViewById(R.id.contactName)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageHolder {
@@ -35,13 +37,16 @@ class ImagesAdapter(private var imageList:MutableList<String>,private var contex
 
     override fun onBindViewHolder(holder: ImageHolder, position: Int) {
 //        shimmerFrameLayout.stopShimmer()
+        holder.contactName.text = imageList[holder.adapterPosition].name
         val handler = Handler(Looper.getMainLooper())
         holder.itemView.apply {
             Thread{
-                val img = downloadImage(imageList[position])
+                val img = downloadImage(imageList[holder.adapterPosition].image)
                 handler.post {
-                    holder.imageView.setImageBitmap(img)
-                    holder.imageView.visibility = View.VISIBLE
+                    if(holder.adapterPosition==position){
+                        holder.imageView.setImageBitmap(img)
+                        holder.imageView.visibility = View.VISIBLE
+                    }
                 }
             }.start()
         }
